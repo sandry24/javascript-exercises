f42d187d1a1da9c2a9d3e12642962fb4a1cb6e2c
sandry24 <sandry.grosu.24@gmail.com>
2024-07-22 17:20:21 +0300

    Complete fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(left, right) {
+    if (!Number.isInteger(left) || !Number.isInteger(right)) {
+        return "ERROR";
+    }
 
+    if (left > right) {
+        [left, right] = [right, left];
+    }
+
+    if (left < 0) {
+        return "ERROR";
+    }
+
+    return right * (right + 1) / 2 - (left - 1) * left / 2;
 };
 
 // Do not edit below this line
