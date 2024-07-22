b6411b48f27266b743e637ff04afd29ed647a3b1
sandry24 <sandry.grosu.24@gmail.com>
2024-07-22 17:26:39 +0300

    Complete sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return false;
+    if (year < 0) return false;
+    
+    if (year % 4) return false;
+    if (year % 100) return true;
+    if (year % 100 == 0 && year % 400 == 0) return true;
+    return false;
 };
 
 // Do not edit below this line
