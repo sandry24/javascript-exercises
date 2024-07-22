e4e95fffd77100b8921a7db63eaf56ed7b4a1153
sandry24 <sandry.grosu.24@gmail.com>
2024-07-22 17:06:40 +0300

    Complete fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    array = args[0];
+    for (let i = 1; i < args.length; i++) {
+        let index = array.indexOf(args[i]);
+        while (index !== -1) {
+            array.splice(index, 1);
+            index = array.indexOf(args[i]);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
