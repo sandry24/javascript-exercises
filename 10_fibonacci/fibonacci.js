e5876c6f28f4d2f59e8e6960466980a10535c945
sandry24 <sandry.grosu.24@gmail.com>
2024-07-31 15:16:52 +0300

    Complete tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
 
+    if (n < 0)
+        return "OOPS";
+    
+    let a = 0;
+    let b = 1;
+
+    for (let i = 0; i < n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return a;
 };
 
 // Do not edit below this line
