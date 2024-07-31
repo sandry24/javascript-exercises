d9825cb6ccb9a661862a5004e1c6665e4171ff3f
sandry24 <sandry.grosu.24@gmail.com>
2024-07-31 15:36:39 +0300

    Complete twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function(person) {
+    let birth = person.yearOfBirth;
+    let death = person.yearOfDeath;
+    if (death === undefined)
+        death = new Date().getFullYear();
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let oldestAge = getAge(oldest);
+        let currentAge = getAge(current);
+        return currentAge > oldestAge ? current : oldest;
+    });
 };
 
 // Do not edit below this line
