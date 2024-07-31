9df794025d0bd3e1844121d049f2f9b5f070b1aa
sandry24 <sandry.grosu.24@gmail.com>
2024-07-31 14:54:19 +0300

    Complete eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let answer = 1;
+  for (let i = 2; i <= a; i++)
+    answer *= i;
+  return answer;
 };
 
 // Do not edit below this line
