bf5ed865e46a2afcc30cb79f0dbf88ade3b634bf
sandry24 <sandry.grosu.24@gmail.com>
2024-07-31 15:08:14 +0300

    Complete ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    s = s
+    .split('')
+    .filter(isAlphanumeric)
+    .join('')
+    .toLowerCase();
+    let n = s.length;
+    for (let i = 0; i <= n/2; i++) 
+        if (s[i] !== s[n - i - 1])
+            return false;
+    return true;
 };
+
+let isAlphanumeric = function(ch) {
+    return ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'));
+}
 
 // Do not edit below this line
 module.exports = palindromes;
